e89e3b26458805b26e58c97729680ca3c17a28e4
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-01-05 20:02:47 +0700

    ♻️ 1.8: Variables name and format

--- UITAutoLecturerSurvey.user.js
+++ UITAutoLecturerSurvey.user.js
@@ -4,7 +4,7 @@
 // @namespace       UIT-KevinNitro
 // @description     Tự động đánh giá khảo sát giảng viên UIT. vui lòng disable script khi không sử dụng, tránh conflict với các khảo sát / link khác của trường
 // @license         https://github.com/KevinNitroG/UIT-Auto-Lecturer-Survey/raw/main/LICENSE
-// @version         1.7
+// @version         1.8
 // @icon            https://github.com/KevinNitroG/UIT-Auto-Lecturer-Survey/raw/main/UIT-logo.png
 // @match           http*://survey.uit.edu.vn/index.php/survey/index/sid/*/token/*
 // @match           http*://survey.uit.edu.vn/index.php/survey/index
@@ -20,25 +20,36 @@
 (function () {
     "use strict";
 
-    function sortArrayRandomly(array) {
-        return array.sort(function () {
-            return Math.random() - 0.5;
-        });
-    }
-
-    function randomIndex(array) {
-        return Math.floor(Math.random() * array.length);
-    }
-
-    // Find all tag with class "answertext"
-    let answerLabels = document.querySelectorAll("label.answertext");
+    // Values for first type questions
     const firstTypeSelectionsArray = [
         ">80%",
         "50-80%",
         "Từ 70 đến dưới 90%",
         "Trên 90%",
     ];
-    // Select first type
+
+    // Value for second type questions
+    const secondTypeSelectionsArray = [
+        // "answer_cell_00MH01",
+        // "answer_cell_00MH02",
+        "answer_cell_00MH03",
+        "answer_cell_00MH04",
+    ];
+
+    // Sort array randomly and return array
+    function sortArrayRandomly(array) {
+        return array.sort(function () {
+            return Math.random() - 0.5;
+        });
+    }
+
+    // Return random index of array
+    function randomIndex(array) {
+        return Math.floor(Math.random() * array.length);
+    }
+
+    // Select first type questions
+    let answerLabels = document.querySelectorAll("label.answertext");
     answerLabels.forEach(function (label) {
         firstTypeSelectionsArray = sortArrayRandomly(firstTypeSelectionsArray);
         for (let i = 0; i < firstTypeSelectionsArray.length; i++) {
@@ -49,19 +60,18 @@
         }
     });
 
-    // Chọn cái bảng - second type
-    const secondTypeSelectionsArray = [
-        // "answer_cell_00MH01",
-        // "answer_cell_00MH02",
-        "answer_cell_00MH03",
-        "answer_cell_00MH04",
-    ].map(function (className) {
-        return className + " answer-item radio-item";
-    });
+    // Select second type questions
+    let secondTypeSelectionsArrayClass = secondTypeSelectionsArray.map(
+        function (className) {
+            return className + " answer-item radio-item";
+        }
+    );
     let radioLists = document.querySelectorAll(".answers-list.radio-list");
     radioLists.forEach(function (radioList) {
         let randomElementClass =
-            secondTypeSelectionsArray[randomIndex(secondTypeSelectionsArray)];
+            secondTypeSelectionsArrayClass[
+                randomIndex(secondTypeSelectionsArrayClass)
+            ];
         let randomElement = radioList.querySelector(
             "." + randomElementClass.replace(/ /g, ".")
         );
@@ -70,11 +80,9 @@
         }
     });
 
-    // Sleep tí
     // setTimeout(function () {}, 100);
 
-    // Check for the button with type="submit" and id="movenextbtn"
-    // Ấn tiếp tục
+    // Continue to next page
     let moveNextBtn = document.querySelector(
         'button[type="submit"][id="movenextbtn"]'
     );
@@ -82,14 +90,13 @@
         moveNextBtn.click();
     }
 
-    // Trigger onclick on the button with id="movesubmitbtn"
-    // Ấn gửi nộp form
+    // Submit form
     let submitBtn = document.getElementById("movesubmitbtn");
     if (submitBtn) {
         submitBtn.click();
     }
 
-    // Close tab if done the form
+    // Close tab when done
     let doneWindow = document.querySelector(".site-name");
     if (doneWindow.innerText.trim() === "HOÀN THÀNH KHẢO SÁT") {
         window.close();
